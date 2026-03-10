e66fcf65644f0db411d84203100326a84f9a50b5
Andrew Soon <andrewsoonqn@gmail.com>
2026-03-10 20:48:51 +0800

    Simplify run script

--- run_models.sh
+++ run_models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python -u DSCNet_3D_opensource/Code/Kipa/DSCNet/S0_Main.py --run_label MVR_Half_Binned_64x64x64_5Offset_Extend1p75_1
-OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python -u DSCNet_3D_opensource/Code/Kipa/DSCNet/S0_Main.py --run_label MVR_Half_Binned_64x64x64_5Offset_Extend1p75_2
-OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python -u DSCNet_3D_opensource/Code/Kipa/DSCNet/S0_Main.py --run_label MVR_Half_Binned_64x64x64_5Offset_Extend1p75_3
+DSCNetEnv/Scripts/activate
+python DSCNet_3D_opensource/Code/Kipa/DSCNet/S0_Main.py --run_label Test_Run
+deactivate
